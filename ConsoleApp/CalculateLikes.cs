a71cbe2218b69c03c50510cb5c4b721dd5530404
huseyn1951 <sultanlkhuseyn@gmail.com>
2024-03-14 14:16:13 +0400

    like

---
+++ ConsoleApp/CalculateLikes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+     public class CalculateLikes
+    {
+        public  void LikeCalculator(int days)
+        {
+            int peopleCount =5;
+            int likeCount = 0;
+ 
+            for (int i = 1; i <= days; i++)
+            {
+                int todayLikes= peopleCount / 2;
+                likeCount += todayLikes;
+                peopleCount = todayLikes * 3;
+            }
+            Console.WriteLine(likeCount); 
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculateLikes calculate = new CalculateLikes();
+            calculate.LikeCalculator(30);
+            
+        }
+    }
+}
